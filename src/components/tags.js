697ac7d28e35d9e6bb6451ce69af2512dad563ac
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 20:44:27 +0000

    tags.js: changed

--- src/components/tags.js
+++ src/components/tags.js
@@ -35,7 +35,7 @@ const Tag = styled.span`
   & a {
     position: relative;
     z-index: 2;
-    background-color: rgba(255, 255, 255, 0.1);
+    background-color: rgba(0, 0, 0, 0.1);
     text-decoration: none;
     color: inherit;
     padding: 0.2rem 0.6rem;
