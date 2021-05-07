4a79aafab6d59c736a89cdf1e28067976022a869
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 19:47:15 +0000

    footer.js: changed

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
 export default Footer;
 
 const StyledFooter = styled.footer`
-  padding-top: var(--size-300);
+  padding-top: var(--size-700);
   padding-bottom: var(--size-300);
 `;
 
