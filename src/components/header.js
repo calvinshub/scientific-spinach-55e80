764548777fd1e434cad122efdb3995ba1900bee9
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 16:13:48 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -26,7 +26,7 @@ const Header = () => {
 
         <HeaderNavList>
           <HeaderNavListItem>
-            <Link to="/blog">Blog</Link>
+            <Link to="/blog"></Link>
           </HeaderNavListItem>
 
           <HeaderNavListItem>
