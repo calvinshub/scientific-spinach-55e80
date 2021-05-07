00ca445636020e1e48d98963c3d843ee1cb8cbab
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 18:24:18 +0000

    blog.js: changed

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -61,7 +61,6 @@ export const homePageQuery = graphql`
         timeToRead
         frontmatter {
           date(formatString: "MMMM DD, YYYY")
-          duration
           description
           title
           tags
