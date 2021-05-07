8f01f8d2bccd40cb9243ca2f8b596917f754bd41
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 19:27:02 +0000

    global-styles.js: changed

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -107,7 +107,7 @@ body {
   background-attachment: fixed;
   background-color: #eaeef4;
   {/* background-image: linear-gradient(225deg, #eaeef4 0%, #FFFFFF 74%); */}
-  background-image: 
+  background-image: url("paper.gif");
 }
 
 h1,
