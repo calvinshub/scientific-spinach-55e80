11ff2ef6b2c48ab8052d3c5a52611ebd09237a90
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 20:52:33 +0000

    post-list.js: changed

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -58,27 +58,7 @@ const PostListItem = ({
   );
 };
 
-const Button = styled.span`
-  margin-right: 0.6rem;
-  margin-bottom: 0.6rem;
-  text-transform: uppercase;
-  font-size: var(--size-300);
 
-  & a {
-    position: relative;
-    z-index: 2;
-    background-color: rgba(0, 0, 0, 0.03);
-    text-decoration: none;
-    color: inherit;
-    padding: 0.2rem 0.6rem;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    border-radius: 4px;
-  }
-
-  & a:hover {
-    background-color: rgba(255, 255, 255, 0.9);
-  }
-;
 
 const StyledPostList = styled.ul`
   padding: 0;
