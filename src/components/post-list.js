715e6484857fb7da8c5424dd0e9720df04f313db
calvinshub <calvinshub@gmail.com>
2021-05-07 15:54:22 +0000

    post-list.js: updated span

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -51,7 +51,7 @@ const PostListItem = ({
       <PostListMeta>
         <span>{date}</span>
 
-        <span>{timeToRead} min</span>
+        <span>{timeToRead} 60 min</span>
       </PostListMeta>
     </StyledPostListItem>
   );
